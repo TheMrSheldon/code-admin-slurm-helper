e75eb0f9bc59bc5811d7c5e5e4fd3b2835c6d049
Tim Hagen <timhagenmail@googlemail.com>
2025-10-30 10:29:39 +0000

    initiale v2

--- svelte.config.js
+++ svelte.config.js
@@ -3,7 +3,6 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	extensions: ['.svelte'],
 	// Consult https://svelte.dev/docs/kit/integrations
 	// for more information about preprocessors
 	preprocess: vitePreprocess(),
